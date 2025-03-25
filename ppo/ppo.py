3906f3d3f46c5f0d54931b5603e7aa2ec35fcec4
wqq <1258552199@qq.com>
2025-03-25 23:22:10 +0800

    udpate

---
+++ ppo/ppo.py
@@ -0,0 +1,52 @@
+
+import torch
+import torch.nn.functional as F
+from torch import nn
+from torch.distributions import Categorical
+
+
+class Actor(nn.Module):
+    def __init__(self, state_dim, num_actions, hidden_dim, dropout = 0.1):
+        super(Actor, self).__init__()
+        
+        self.proj_in = nn.Sequential(
+            nn.Linear(state_dim, hidden_dim),
+            nn.ReLU(), 
+            nn.Dropout(dropout),
+            nn.Linear(hidden_dim, hidden_dim),
+            nn.ReLU()
+        )
+
+        self.action_head = nn.Sequential(
+            nn.Linear(hidden_dim * 2, hidden_dim),
+            nn.ReLU(),
+            nn.Linear(hidden_dim, num_actions), 
+        )
+
+
+    def forward(self, state):
+        x = self.proj_in(state)
+        x = self.action_head(x)
+        return x
+
+class Critic(nn.Module):
+    def __init__(self, state_dim, hidden_dim, dim_pred = 1, dropout = 0.1):
+        super(Critic, self).__init__()
+        
+        self.proj_in = nn.Sequential(
+            nn.Linear(state_dim, hidden_dim),
+            nn.ReLU(), 
+            nn.Dropout(dropout),
+            nn.Linear(hidden_dim, hidden_dim),
+            nn.ReLU()
+        )
+
+        self.value_head = nn.Linear(hidden_dim, dim_pred)
+
+    def forward(self, state):
+        x = self.proj_in(state)
+        x = self.value_head(x)
+        return x
+
+
+
--- train.py
+++ train.py
@@ -1,20 +1,74 @@
 import gymnasium as gym
+from tqdm import tqdm
+from dataclasses import dataclass
+import torch
+import torch.nn.functional as F
+from collections import deque
+from torch.distributions import Categorical
+from 
 
-# Initialise the environment
-env = gym.make("BipedalWalker-v3", hardcore=True, render_mode="human")
+from ppo.ppo import Actor, Critic
 
-# Reset the environment to generate the first observation
-observation, info = env.reset(seed=42)
-for _ in range(1000):
-    # this is where you would insert your policy
-    action = env.action_space.sample()
 
-    # step (transition) through the environment with the action
-    # receiving the next observation, reward and if the episode has terminated or truncated
-    observation, reward, terminated, truncated, info = env.step(action)
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
-    # If the episode has ended then we can reset to start a new episode
-    if terminated or truncated:
-        observation, info = env.reset()
+@dataclass
+class Buffer: 
+    ## (s_t, a_t, r_t, s_t+1, log_pi_t, done_t, V_t)
+    states:  torch.Tensor
+    actions: torch.Tensor
+    reward: torch.Tensor
+    next_state: torch.Tensor
+    action_log_prob: torch.Tensor
+    done: torch.Tensor
+    value: torch.Tensor
 
-env.close()
+
+class Agent:
+    def __init__(self, env, actor_hidden_dim = 64, critic_hidden_dim = 256):
+        self.env = env
+        self.state_dim = env.observation_space.shape[0]
+        self.num_actions = env.action_space.n
+        self.actor = Actor(self.state_dim, self.num_actions, actor_hidden_dim).to(device)
+        self.critic = Critic(self.state_dim, critic_hidden_dim).to(device)
+    
+
+    @torch.no_grad()
+    def get_action(self, state):
+        action_probs = self.actor(state)
+        dist = Categorical(action_probs)
+        action = dist.sample()
+        action_log_prob = dist.log_prob(action)
+        return action.item(), action_log_prob
+    
+if __name__ == "__main__":
+    num_episodes = 50000
+    update_timesteps = 2048
+    max_timesteps = 500
+
+    # Initialise the environment
+    env = gym.make("BipedalWalker-v3", hardcore=True, render_mode="rgb_array")
+
+    env = gym.wrappers.RecordVideo(
+            env = env,
+            video_folder = "./videos",
+            name_prefix = 'lunar-video',
+            episode_trigger = lambda eps_num: eps_num % 100 == 0,
+            disable_logger = True
+        )
+
+    env = gym.wrappers.RecordEpisodeStatistics(env, buffer_length=num_episodes)
+
+    agent = Agent(env)
+    time = 0
+    replay_buffer = deque([])
+
+    for eps in tqdm(range(num_episodes), desc = 'episodes'):
+
+        for timestep in range(max_timesteps):
+            time += 1
+            state = torch.from_numpy(state).to(device)
+
+    # Close the environment
+    env.close()
+
