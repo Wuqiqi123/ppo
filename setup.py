83d24601995ebe8ebd5aed527116722ee18f542b
wqq <1258552199@qq.com>
2025-03-27 21:08:37 +0800

    udpate

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+  name = 'ppo',
+  packages = find_packages(exclude=[]),
+  version = '0.0.1',
+  license='MIT',
+  description = 'ppo implementation in pytorch',
+  author = 'Qiqi Wu',
+  author_email = '1258552199@qq.com',
+  long_description_content_type = 'text/markdown',
+  url = 'https://github.com/Wuqiqi123/ppo',
+  keywords = ['ppo', 'pytorch']
+)
