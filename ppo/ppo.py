42bcb758f12100bfbb82c56a0be3a961b057e937
wqq <1258552199@qq.com>
2025-03-28 23:34:07 +0800

    udpate

--- ppo/ppo.py
+++ ppo/ppo.py
@@ -1,4 +1,3 @@
-
 import torch
 import torch.nn.functional as F
 from torch import nn
@@ -18,16 +17,27 @@ class Actor(nn.Module):
         )
 
         self.action_head = nn.Sequential(
-            nn.Linear(hidden_dim * 2, hidden_dim),
+            nn.Linear(hidden_dim, hidden_dim),
             nn.ReLU(),
             nn.Linear(hidden_dim, num_actions), 
         )
 
-
+    @torch.no_grad()
+    def forward_eval(self, *args, **kwargs):
+        training = self.training
+        self.eval()
+        out = self.forward(*args, **kwargs)
+        self.train(training)
+        return out
+    
     def forward(self, state):
+        '''
+            state: [batch_size, state_dim]
+            return: [batch_size, num_actions]
+        '''
         x = self.proj_in(state)
         x = self.action_head(x)
-        return x
+        return F.softmax(x, dim=-1)  # Normalize logits to probabilities
 
 class Critic(nn.Module):
     def __init__(self, state_dim, hidden_dim, dim_pred = 1, dropout = 0.1):
--- train.py
+++ train.py
@@ -5,7 +5,8 @@ import torch
 import torch.nn.functional as F
 from collections import deque
 from torch.distributions import Categorical
-from 
+from ppo.ema import EMA
+import numpy as np
 
 from ppo.ppo import Actor, Critic
 
@@ -14,24 +15,45 @@ device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
 @dataclass
 class Buffer: 
-    ## (s_t, a_t, r_t, s_t+1, log_pi_t, done_t, V_t)
+    ## (s_t, a_t, r_t, s_t+1, log_pi_t, terminated, V_t)
     states:  torch.Tensor
     actions: torch.Tensor
     reward: torch.Tensor
     next_state: torch.Tensor
     action_log_prob: torch.Tensor
-    done: torch.Tensor
+    terminated: torch.Tensor
     value: torch.Tensor
 
 
-class Agent:
-    def __init__(self, env, actor_hidden_dim = 64, critic_hidden_dim = 256):
+class PPOAgent:
+    def __init__(self, env, actor_hidden_dim = 64, critic_hidden_dim = 256,
+            ppo_clip = 0.2,
+            ppo_epochs = 4,
+            ppo_batch_size = 64,
+            ppo_lr = 3e-4,
+            ppo_eps = 1e-5,
+            gae_lambda = 0.98,
+            entropy_coef = 0.01,
+    ):
         self.env = env
+        self.ppo_clip = ppo_clip
+        self.ppo_epochs = ppo_epochs
+        self.ppo_batch_size = ppo_batch_size
+        self.ppo_lr = ppo_lr
+        self.ppo_eps = ppo_eps
+        self.gae_lambda = gae_lambda
+        self.entropy_coef = entropy_coef
+
         self.state_dim = env.observation_space.shape[0]
         self.num_actions = env.action_space.n
         self.actor = Actor(self.state_dim, self.num_actions, actor_hidden_dim).to(device)
         self.critic = Critic(self.state_dim, critic_hidden_dim).to(device)
-    
+        self.optimizer_actor = torch.optim.Adam(self.actor.parameters(), lr = 3e-4, eps = 1e-5)
+        self.optimizer_critic = torch.optim.Adam(self.critic.parameters(), lr = 3e-4, eps = 1e-5)
+        self.target_critic = EMA(self.critic)
+        self.target_critic.add_to_optimizer_post_step_hook(self.optimizer_critic)
+        
+        
 
     @torch.no_grad()
     def get_action(self, state):
@@ -40,34 +62,61 @@ class Agent:
         action = dist.sample()
         action_log_prob = dist.log_prob(action)
         return action.item(), action_log_prob
+
+    def optimize(self, memories, next_state):
+        pass
     
 if __name__ == "__main__":
     num_episodes = 50000
     update_timesteps = 2048
-    max_timesteps = 500
+    max_timesteps = 1000
 
     # Initialise the environment
-    env = gym.make("BipedalWalker-v3", hardcore=True, render_mode="rgb_array")
+    env = gym.make("LunarLander-v3", render_mode="rgb_array")
 
     env = gym.wrappers.RecordVideo(
             env = env,
             video_folder = "./videos",
-            name_prefix = 'lunar-video',
+            name_prefix = 'video',
             episode_trigger = lambda eps_num: eps_num % 100 == 0,
             disable_logger = True
         )
 
     env = gym.wrappers.RecordEpisodeStatistics(env, buffer_length=num_episodes)
 
-    agent = Agent(env)
+
+    agent = PPOAgent(env)
     time = 0
     replay_buffer = deque([])
+    num_policy_updates = 0
 
     for eps in tqdm(range(num_episodes), desc = 'episodes'):
-
+        state, info = env.reset()
         for timestep in range(max_timesteps):
             time += 1
+            # import ipdb; ipdb.set_trace()   
             state = torch.from_numpy(state).to(device)
+            action_probs = agent.actor.forward_eval(state)
+            value = agent.target_critic.forward_eval(state)
+            dist = Categorical(action_probs)
+            action = dist.sample()
+            action_log_prob = dist.log_prob(action)
+            action = action.item()
+            
+            next_state, reward, terminated, truncated, info = env.step(action)
+
+            buffer = Buffer(state, action, reward, next_state, action_log_prob, terminated, value)
+            replay_buffer.append(buffer)
+
+            state = next_state
+
+            if time % update_timesteps == 0:
+                agent.optimize(replay_buffer, next_state)
+                num_policy_updates += 1
+                replay_buffer.clear()
+
+            if terminated:
+                break
 
     # Close the environment
     env.close()
